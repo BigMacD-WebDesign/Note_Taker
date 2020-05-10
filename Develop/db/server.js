4bb199546118869011c0e165271910cd83b1ce6f
Scott MacDonald <scottmacd88@yahoo.com>
2020-05-10 12:08:56 -0700

    Added server.js and attempted to add listening server

---
+++ Develop/db/server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+
+
+
+
+app.listen(PORT, function () {
+    console.log(`App listening on http://localhost:${PORT}`);
+})
