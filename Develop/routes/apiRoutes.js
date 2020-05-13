f986a06e572d91789f4bcfa2db8f9f0c3f327430
Scott MacDonald <scottmacd88@yahoo.com>
2020-05-12 21:58:31 -0700

    Resolved data display. Notes now generate.

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,18 +1,19 @@
 var noteData = require("../db/db.json");
-
+// var records = [];
 const { v4: uuidv4 } = require('uuid');
 var fs = require("fs")
-
+var records = [];
 
 module.exports = function(app) {
 
   app.get("/api/notes", function(req, res) {
-    //   fs.readFile("../db/db.json", function(err,data){
-    //     console.log("Get route",err,data)
-    //     noteData = JSON.parse(data)
-    //     res.json(noteData);
-    //   });
-    // res.json(data)
+      fs.readFile("./db/db.json", "utf-8", function(err,data){
+        console.log("Get route",err,data)
+        // noteData = JSON.parse(data)
+        res.json(JSON.parse(data));
+        
+      });
+  
    
   });
 
@@ -22,20 +23,22 @@ module.exports = function(app) {
 
   app.post("/api/notes", function(req, res) {
    
-    var records = {
+    var record = {
         id:  uuidv4(),
         title:req.body.title,
         text:req.body.text
     }
+    
+    records.push(record);
 
     // noteData.push(records.body);
       // data.push(records)
-    fs.writeFile("../db/db.json", records, "utf-8", function(err, data) {
+    fs.writeFile("./db/db.json", JSON.stringify(records), "utf-8", function(err, data) {
         if(err){
             throw err
         }
         console.log(data);
-        res.json("Succesfully written");
+        res.send("Succesfully written");
     });
         
       // noteData.push(records);
@@ -45,12 +48,14 @@ module.exports = function(app) {
 
 
 
+
+
  
 
   app.delete("/api/note/:id", function(req, res) {
     // Empty out the arrays of data
     console.log(req.params.id);
-
+    fs.readFile("")
     res.json({ ok: true });
   });
 };
