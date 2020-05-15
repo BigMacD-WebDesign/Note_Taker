137eb21f437e336e9ad5f516caa6aa12d76947ba
Scott MacDonald <scottmacd88@yahoo.com>
2020-05-14 21:54:24 -0700

    Figured out issue with deleting notes. Test successful

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,61 +1,80 @@
 var noteData = require("../db/db.json");
-// var records = [];
 const { v4: uuidv4 } = require('uuid');
 var fs = require("fs")
 var records = [];
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-  app.get("/api/notes", function(req, res) {
-      fs.readFile("./db/db.json", "utf-8", function(err,data){
-        console.log("Get route",err,data)
-        // noteData = JSON.parse(data)
-        res.json(JSON.parse(data));
-        
-      });
-  
-   
+  app.get("/api/notes", function (req, res) {
+    fs.readFile("./db/db.json", "utf-8", function (err, data) {
+
+      res.json(JSON.parse(data));
+
+    });
+
+
   });
 
 
 
-  
 
-  app.post("/api/notes", function(req, res) {
-   
+
+  app.post("/api/notes", function (req, res) {
+
     var record = {
-        id:  uuidv4(),
-        title:req.body.title,
-        text:req.body.text
+      id: uuidv4(),
+      title: req.body.title,
+      text: req.body.text
     }
-    
+
     records.push(record);
 
-    // noteData.push(records.body);
-      // data.push(records)
-    fs.writeFile("./db/db.json", JSON.stringify(records), "utf-8", function(err, data) {
-        if(err){
-            throw err
-        }
-        console.log(data);
-        res.send("Succesfully written");
+    fs.writeFile("./db/db.json", JSON.stringify(records), "utf-8", function (err, data) {
+      if (err) {
+        throw err
+      }
+      
+      res.send("Succesfully written");
     });
-        
-      // noteData.push(records);
-      // res.json(noteData);
-    
+
   });
 
 
 
 
 
- 
 
-  app.delete("/api/note/:id", function(req, res) {
-    // Empty out the arrays of data
+
+  app.delete("/api/notes/:id", function (req, res) {
+
     console.log(req.params.id);
-    fs.readFile("")
-    res.json({ ok: true });
+    var noteId = req.params.id;
+
+    fs.readFile("./db/db.json", "utf-8", function (err, data) {
+
+      let parsedData = JSON.parse(data);
+
+
+      for (i = 0; i < parsedData.length; i++) {
+
+        if (parsedData[i].id === noteId) {
+          console.log("id found");
+          parsedData.splice(i, 1);
+
+        }
+        console.log(parsedData[i]);
+      }
+
+      fs.writeFile("./db/db.json", JSON.stringify(parsedData), "utf-8", function (err, data) {
+        if (err) {
+          throw err
+        }
+        console.log(parsedData);
+        res.send("Succesfully written");
+      });
+
+    });
+
   });
+
 };
